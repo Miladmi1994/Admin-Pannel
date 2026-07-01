c6b15b489ba0634312139b3cad6a0db67cbada9a
DESKTOP-T3GTJNV\oto <zahrakrad76@gmail.com>
2026-07-01 16:52:11 +0330

    test deploy

--- src/pages/Admins.tsx
+++ src/pages/Admins.tsx
@@ -29,6 +29,7 @@ export default function Admins() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        
         {ADMINS.map((admin, i) => (
           <motion.div
             key={i}
